1068426589a543303d4ac4346564e9b8058829cd
PawelGebal <pawel.gebal@reef.pl>
2026-09-17 14:37:37 +0200

    docs: remove redundant parking comment

--- src/AlphaVault.sol
+++ src/AlphaVault.sol
@@ -102,7 +102,6 @@ contract AlphaVault is ERC1155, ERC1155Supply, ReentrancyGuard, IAlphaVaultAbi {
     }
 
     /// @notice Whether deposits and weight alignment wait for an attestation newer than the parking one.
-    /// @dev Alpha may stay parked until the next wrap, rebalance or alpha exit.
     function awaitingAttestation(uint256 tokenId) public view returns (bool) {
         uint256 parkedAtNonce = recovery[tokenId].parkedAtNonce;
         return parkedAtNonce != 0 && validatorRegistry.nonces(VaultMath.netuidOf(tokenId)) == parkedAtNonce;
